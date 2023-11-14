a1e4207a1990f6787f73c8a5041348c80750efe9
seohokim <shk9946@gmail.com>
2023-11-14 14:55:52 +0900

    [FEAT] survey 쿼리, create mutation 구현

--- src/survey/survey.resolver.ts
+++ src/survey/survey.resolver.ts
@@ -1,4 +1,23 @@
-import { Resolver } from '@nestjs/graphql';
+import { Resolver, Query, Mutation, Args, Int } from '@nestjs/graphql';
+import { SurveyService } from './survey.service';
+import { SurveyType } from './types/survey.type';
+import { CreateSurveyInputDto } from './dto/create-surver.dto';
 
-@Resolver()
-export class SurveyResolver {}
+@Resolver((of) => SurveyType)
+export class SurveyResolver {
+  constructor(private surveyService: SurveyService) {}
+
+  @Query((returns) => SurveyType)
+  async survey(@Args('id', { type: () => Int }) id: number) {
+    return this.surveyService.findOne(id);
+  }
+
+  @Mutation((returns) => SurveyType)
+  async createSurvey(
+    @Args('createSurveyInput') createSurveyInput: CreateSurveyInputDto,
+  ) {
+    return this.surveyService.create(createSurveyInput);
+  }
+
+  // 기타 필요한 Query와 Mutation 추가
+}
