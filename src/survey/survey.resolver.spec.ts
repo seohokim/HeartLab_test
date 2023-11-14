50a22a903604912a3b8926c85eff73cd831d74e1
seohokim <shk9946@gmail.com>
2023-11-14 18:57:30 +0900

    [TEST] survey resolver, service 테스트 코드 작성

--- src/survey/survey.resolver.spec.ts
+++ src/survey/survey.resolver.spec.ts
@@ -1,18 +1,183 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { SurveyResolver } from './survey.resolver';
+import { SurveyService } from './survey.service';
+import {
+  CreateSurveyInputDto,
+  CreateSurveyOutputDto,
+} from './dto/create-survey.dto';
+import { GetSurveyInputDto, GetSurveyOutputDto } from './dto/get-survey.dto';
+import {
+  UpdateSurveyInputDto,
+  UpdateSurveyOutputDto,
+} from './dto/update-survey.dto';
+import {
+  RemoveSurveyInputDto,
+  RemoveSurveyOutputDto,
+} from './dto/remove-survey.dto';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Survey } from './entities/survey.entity';
+import { Repository } from 'typeorm';
 
 describe('SurveyResolver', () => {
   let resolver: SurveyResolver;
-
+  let service: SurveyService;
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [SurveyResolver],
+      providers: [
+        SurveyResolver,
+        SurveyService,
+        {
+          provide: getRepositoryToken(Survey),
+          useClass: Repository,
+        },
+      ],
     }).compile();
 
     resolver = module.get<SurveyResolver>(SurveyResolver);
+    service = module.get<SurveyService>(SurveyService);
   });
 
   it('should be defined', () => {
     expect(resolver).toBeDefined();
+  });
+
+  describe('readSurvey', () => {
+    it('should return a survey', async () => {
+      const survey = {
+        id: 1,
+        title: 'Test Survey',
+        description: 'Test Description',
+        questions: [],
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      };
+      jest.spyOn(service, 'findOne').mockImplementation(async () => ({
+        ok: true,
+        statusCode: 200,
+        survey,
+      }));
+
+      const getSurveyInput: GetSurveyInputDto = { id: 1 };
+      const result: GetSurveyOutputDto =
+        await resolver.readSurvey(getSurveyInput);
+
+      expect(service.findOne).toHaveBeenCalledWith(getSurveyInput);
+      expect(result).toEqual({ ok: true, statusCode: 200, survey });
+    });
+  });
+
+  describe('readSurveys', () => {
+    it('should return an array of surveys', async () => {
+      const surveys = [
+        {
+          id: 1,
+          title: 'Test Survey 1',
+          description: 'Test Description 1',
+          questions: [],
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          id: 2,
+          title: 'Test Survey 2',
+          description: 'Test Description 2',
+          questions: [],
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+      ];
+      jest.spyOn(service, 'findAll').mockImplementation(async () => ({
+        ok: true,
+        statusCode: 200,
+        surveys,
+      }));
+
+      const result = await resolver.readSurveys();
+
+      expect(service.findAll).toHaveBeenCalled();
+      expect(result).toEqual({ ok: true, statusCode: 200, surveys });
+    });
+  });
+
+  describe('createSurvey', () => {
+    it('should create a survey', async () => {
+      const survey = {
+        id: 1,
+        title: 'Test Survey',
+        description: 'Test Description',
+        questions: [],
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      };
+      jest.spyOn(service, 'create').mockImplementation(async () => ({
+        ok: true,
+        statusCode: 200,
+        survey,
+      }));
+
+      const createSurveyInput: CreateSurveyInputDto = {
+        title: 'Test Survey',
+        description: 'Test Description',
+      };
+      const result: CreateSurveyOutputDto =
+        await resolver.createSurvey(createSurveyInput);
+
+      expect(service.create).toHaveBeenCalledWith(createSurveyInput);
+      expect(result).toEqual({ ok: true, statusCode: 200, survey });
+    });
+  });
+
+  describe('updateSurvey', () => {
+    it('should update a survey', async () => {
+      const survey = {
+        id: 1,
+        title: 'Test Survey',
+        description: 'Test Description',
+        questions: [],
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      };
+      jest.spyOn(service, 'update').mockImplementation(async () => ({
+        ok: true,
+        statusCode: 200,
+        survey,
+      }));
+
+      const updateSurveyInput: UpdateSurveyInputDto = {
+        id: 1,
+        changedTitle: 'Updated Test Survey',
+        changedDescription: 'Updated Test Description',
+      };
+      const result: UpdateSurveyOutputDto =
+        await resolver.updateSurvey(updateSurveyInput);
+
+      expect(service.update).toHaveBeenCalledWith(updateSurveyInput);
+      expect(result).toEqual({ ok: true, statusCode: 200, survey });
+    });
+  });
+
+  describe('removeSurvey', () => {
+    it('should remove a survey', async () => {
+      const survey = {
+        id: 1,
+        title: 'Test Survey',
+        description: 'Test Description',
+        questions: [],
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      };
+      jest.spyOn(service, 'remove').mockImplementation(async () => ({
+        ok: true,
+        statusCode: 200,
+        survey,
+      }));
+
+      const removeSurveyInput: RemoveSurveyInputDto = { id: 1 };
+      const result: RemoveSurveyOutputDto =
+        await resolver.removeSurvey(removeSurveyInput);
+
+      expect(service.remove).toHaveBeenCalledWith(removeSurveyInput);
+      expect(result).toEqual({ ok: true, statusCode: 200, survey });
+    });
   });
 });
--- src/survey/survey.resolver.ts
+++ src/survey/survey.resolver.ts
@@ -1,23 +1,57 @@
 import { Resolver, Query, Mutation, Args, Int } from '@nestjs/graphql';
 import { SurveyService } from './survey.service';
 import { SurveyType } from './types/survey.type';
-import { CreateSurveyInputDto } from './dto/create-surver.dto';
+import {
+  CreateSurveyInputDto,
+  CreateSurveyOutputDto,
+} from './dto/create-survey.dto';
+import {
+  GetSurveyInputDto,
+  GetSurveyOutputDto,
+  GetSurveysOutputDto,
+} from './dto/get-survey.dto';
+import {
+  UpdateSurveyInputDto,
+  UpdateSurveyOutputDto,
+} from './dto/update-survey.dto';
+import {
+  RemoveSurveyInputDto,
+  RemoveSurveyOutputDto,
+} from './dto/remove-survey.dto';
 
 @Resolver((of) => SurveyType)
 export class SurveyResolver {
   constructor(private surveyService: SurveyService) {}
 
   @Query((returns) => SurveyType)
-  async survey(@Args('id', { type: () => Int }) id: number) {
-    return this.surveyService.findOne(id);
+  async readSurvey(
+    @Args('getSurveyInput') getSurveyInput: GetSurveyInputDto,
+  ): Promise<GetSurveyOutputDto> {
+    return this.surveyService.findOne(getSurveyInput);
   }
 
+  @Query((returns) => [SurveyType])
+  async readSurveys(): Promise<GetSurveysOutputDto> {
+    return this.surveyService.findAll();
+  }
   @Mutation((returns) => SurveyType)
   async createSurvey(
     @Args('createSurveyInput') createSurveyInput: CreateSurveyInputDto,
-  ) {
+  ): Promise<CreateSurveyOutputDto> {
     return this.surveyService.create(createSurveyInput);
   }
 
-  // 기타 필요한 Query와 Mutation 추가
+  @Mutation((returns) => SurveyType)
+  async updateSurvey(
+    @Args('updateSurveyInput') updateSurveyInput: UpdateSurveyInputDto,
+  ): Promise<UpdateSurveyOutputDto> {
+    return this.surveyService.update(updateSurveyInput);
+  }
+
+  @Mutation((returns) => SurveyType)
+  async removeSurvey(
+    @Args('updateSurveyInput') removeSurveyInput: RemoveSurveyInputDto,
+  ): Promise<RemoveSurveyOutputDto> {
+    return this.surveyService.remove(removeSurveyInput);
+  }
 }
