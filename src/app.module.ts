79923dd628613fc6a9d1ff2daf3ba3e8b959fd34
seohokim <shk9946@gmail.com>
2023-11-14 14:20:37 +0900

    [FEAT] validation pipe, ORM setting

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,17 @@ import { SurveyModule } from './survey/survey.module';
 import { QuestionModule } from './question/question.module';
 import { OptionModule } from './option/option.module';
 import { ResponseModule } from './response/response.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { typeORMConfig } from './config/typeorm.config';
 
 @Module({
-  imports: [SurveyModule, QuestionModule, OptionModule, ResponseModule],
+  imports: [
+    SurveyModule,
+    QuestionModule,
+    OptionModule,
+    ResponseModule,
+    TypeOrmModule.forRoot(typeORMConfig),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
