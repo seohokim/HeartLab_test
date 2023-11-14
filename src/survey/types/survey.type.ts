546d0f8f343b11c7ab9e6bded21d40a55c5ab10e
seohokim <shk9946@gmail.com>
2023-11-14 19:00:05 +0900

    [STYLE] 상대경로로 변경

--- src/survey/types/survey.type.ts
+++ src/survey/types/survey.type.ts
@@ -1,13 +1,20 @@
 import { ObjectType, Field, Int } from '@nestjs/graphql';
-import { QuestionType } from 'src/question/types/question.type';
+import { IsNumber, IsString } from 'class-validator';
+import { QuestionType } from '../../question/types/question.type';
 
 @ObjectType()
 export class SurveyType {
   @Field(() => Int)
+  @IsNumber()
   id: number;
 
   @Field()
+  @IsString()
   title: string;
+
+  @Field()
+  @IsString()
+  description: string;
 
   @Field(() => [QuestionType], { nullable: true })
   questions?: QuestionType[];
