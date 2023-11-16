f6f14456ba0af41e8b335d273271c2c23120a139
seohokim <shk9946@gmail.com>
2023-11-16 15:52:48 +0900

    [REMOVE] test 삭제

--- src/response/response.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from '@nestjs/common';
-import { ResponseResolver } from './response.resolver';
-import { ResponseService } from './response.service';
-
-@Module({
-  providers: [ResponseResolver, ResponseService]
-})
-export class ResponseModule {}
