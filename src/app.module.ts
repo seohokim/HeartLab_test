d59c56f797c6f723c2e78503c86847a9d6f9537e
seohokim <shk9946@gmail.com>
2023-11-16 15:47:05 +0900

    [FEAT] GraphQl 모듈 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,22 +1,26 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { SurveyModule } from './survey/survey.module';
 import { QuestionModule } from './question/question.module';
 import { OptionModule } from './option/option.module';
-import { ResponseModule } from './response/response.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { typeORMConfig } from './config/typeorm.config';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { AnswerModule } from './answer/answer.module';
 
 @Module({
   imports: [
     SurveyModule,
     QuestionModule,
     OptionModule,
-    ResponseModule,
     TypeOrmModule.forRoot(typeORMConfig),
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: 'schema.gpl',
+    }),
+    AnswerModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
